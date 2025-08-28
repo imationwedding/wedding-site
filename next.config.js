8a3f6794f501b466c870303966344f20443dcce5
Gábor <zeromineday@gmail.com>
2025-08-28 17:11:59 +0200

    ez lesz

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // ide jöhetnek további beállítások, pl. images, rewrites, stb.
 };
 
-export default nextConfig;
+module.exports = nextConfig;
